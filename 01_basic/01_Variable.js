843726b2486ce7db1b6857b619d33049bdea8717
Rohit Patel <rohitppttel@gmail.com>
2024-09-28 11:03:43 +0000

    data  type

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,22 @@
+const accountId=848974242; 
+let accountEmail="example@gmail.com"// scope variable  
+var accountPassword=123456;// globel variable 
+accountcity="Bangalore"// can assign data without using var type but its not pref
+let accountState;// undefind value 
+// accountId=2; // not allowed we can't change the const variable 
+
+
+
+accountEmail="hcge@gmail.com"
+accountPassword="265255";
+accountcity="Dehli"
+
+// console.log(accountId);
+
+/*
+    Prefer not to use var cause of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])  // using table we can console multiple data
+
+
---
+++ 01_basic/02_Datatype.js
@@ -0,0 +1,30 @@
+"use strict"// treate all js code as newer version
+
+// alert(3+2); // we are using node js not browser
+
+console.log(3
+    +
+    3)//code readability should be high
+
+// for js documentation tc39 and MDM
+
+
+let name="Rohit"
+let age =25;
+let isLogIn=true;
+
+
+// number =>2 to power 53 ( range )
+// bigint ( for long range)
+// string =>""
+// boolean=> true/false
+// null ( stand alone value)
+// undefined => 
+// symbol => ( find the uniqueness)
+
+
+// object 
+
+// console.log(typeof age) 
+console.log(typeof null);// object 
+console.log(typeof undefined);
