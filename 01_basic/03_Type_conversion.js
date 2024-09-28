662ae443e331c8329d14228876001a854330ef2a
Rohit Patel <rohitppttel@gmail.com>
2024-09-28 11:38:02 +0000

    data  type coversion

---
+++ 01_basic/03_Type_conversion.js
@@ -0,0 +1,54 @@
+// Datatype coversion 
+let age = "23";// when we number inside the string its conver into number using type conversion 
+
+let score = "23hdshs";// but when we pass number+string then its convert the number but in values its ots (NaN) output
+
+let undo=undefined;// when we pass undefined then its convert the number but in values its ots (NaN) output
+
+let Nul=null// when we pass null then its convert the number but in values its ots (0) output
+
+
+let bool=true;// when we pass Boolean (true) then its convert the number but in values its ots (1) output if we pass false its gives (0) as an output
+
+
+
+// let valueNumber=Number(bool)
+
+// console.log(typeof valueNumber);
+
+// console.log(valueNumber);
+
+
+// for boolean 
+
+let isLoggedIn=1 // output => true
+
+let isLoggedInString="Rohit"
+
+let booleanIsLoggenIn=Boolean(isLoggedInString)
+
+// console.log(typeof booleanIsLoggenIn);
+// console.log(booleanIsLoggenIn);
+
+// 1=> true; 0=> false
+// ""=> false 
+//"Rohit"=>true
+
+
+// for string 
+
+let Somenumber=33; // ouput => 33 
+
+let stringNumber=String(Somenumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+
+
+
+
+
+
+
