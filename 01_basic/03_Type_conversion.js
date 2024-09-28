19b83ab1cf9abe410557c6e9ea292f6ef54926cc
Rohit Patel <rohitppttel@gmail.com>
2024-09-28 12:18:11 +0000

    typeconvertion

--- 01_basic/03_Type_conversion.js
+++ 01_basic/03_Type_conversion.js
@@ -41,8 +41,46 @@ let Somenumber=33; // ouput => 33
 
 let stringNumber=String(Somenumber)
 
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// ******************Operations**************
+
+let value=3 // output=> -3
+let negValue=-value;
+// console.log(negValue);
+
+let str1="Rohit"
+let str2="patel"
+
+let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1"+2);// 12
+// console.log(1+"2") // 12
+// console.log("1"+2+2) //122
+// console.log(1+2+"2") //32
+
+// console.log(+true);// 1
+// console.log(+"");// 0
+
+
+ let gameCounter=100
+ gameCounter++;
+ console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
